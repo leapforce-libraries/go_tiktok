159ab9bab6df49c563eaa76337feeb25e236301f
caselongo <caselongo@gmail.com>
2022-12-07 11:43:08 +0100

    x

--- Service.go
+++ Service.go
@@ -105,7 +105,7 @@ func (service *Service) ValidateToken() (*go_token.Token, *errortools.Error) {
 }
 
 func (service *Service) GetTokenFromCode(r *http.Request) *errortools.Error {
-	return service.oAuth2Service.GetTokenFromCode(r, nil, false)
+	return service.oAuth2Service.GetTokenFromCodeWithoutRedirectUri(r, nil)
 }
 
 func (service *Service) url(path string) string {
